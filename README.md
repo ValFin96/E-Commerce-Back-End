3c60a19c04399340fa18fd1e10dca4b0bdc7676a
Valeriya <valeriya.finogeeva@gmail.com>
2023-02-23 22:06:20 +1100

    Add README file

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # E-Commerce-Back-End
+
+## Description
+
+This project is a back end for an e-commerce site built using Express.js and Sequelize. It provides API routes for managing categories, products, and tags.
+
+## Technologies Used
+
+This project uses the following technologies:
+
+- Node.js
+- Express.js
+- Sequelize
+- MySQL
+
+## Getting STarted
+
+To get started with this project, follow the steps below:
+
+1. Clone the project repository using the command git clone teh repository.
+2. Navigate to the project directory using the command
+
+```
+cd e-commerce-back-end
+```
+
+3. Install the necessary dependencies using the command
+
+```
+npm install
+```
+
+4. Create a .env file and add your database name, MySQL username, and MySQL password.
+5. Run the schema and seed commands to create the development database and seed it with test data using the command
+
+```
+npm run seed
+```
+
+6. Start the application using the command
+
+```
+npm start
+```
+
+## API Routes
+
+This project provides the following API routes:
+
+- /api/categories - provides a list of all categories
+- /api/categories/:id - provides details for a specific category
+- /api/products - provides a list of all products with their associated category and tag data
+- /api/products/:id - provides details for a specific product with its associated category and tag data
+- /api/tags - provides a list of all tags
+- /api/tags/:id - provides details for a specific tag
+
+## Contribution Guidelines
+
+If you want to contribute to this project, please follow the guidelines below:
+
+1. Fork the repository
+2. Create a new branch for your feature or bug fix
+3. Make changes and commit them
+4. Push your changes to your forked repository
+5. Submit a pull request with a detailed description of your changes
+
+## Walkthrough Video
+
+Please follow to watch the [walkthrough video](https://drive.google.com/file/d/1Di5wQEn6US3yL5yX2PLaB37_ns3sE5Kt/view) that demonstrates the functionality of the application.
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -41,19 +41,19 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
             tag_id,
           };
         });
-        return ProductTag.bulkCreate(productTagIdArr);
+        ProductTag.bulkCreate(productTagIdArr)
+          .then((productTagIdArr) => res.status(200).json(productTagIdArr))
       }
       // if no product tags, just respond
       res.status(200).json(product);
     })
-    .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
@@ -62,42 +62,36 @@ router.post('/', (req, res) => {
 
 // update product
 router.put('/:id', (req, res) => {
-  // update product data
   Product.update(req.body, {
     where: {
       id: req.params.id,
     },
   })
     .then((product) => {
-      // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
-    })
-    .then((productTags) => {
-      // get list of current tag_ids
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
-          return {
-            product_id: req.params.id,
-            tag_id,
-          };
+      if (req.body.tagIds && req.body.tagIds.length) {
+        const productTags = ProductTag.findAll({
+          where: { product_id: req.params.id }
         });
-      // figure out which ones to remove
-      const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
-
-      // run both actions
-      return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
+        const productTagIds = productTags.map(({ tag_id }) => tag_id);
+        const newProductTags = req.body.tagIds.filter((tag_id) => !productTagIds.includes(tag_id))
+          .map((tag_id) => {
+            return {
+              product_id: req.params.id,
+              tag_id,
+            };
+          });
+        const productTagsToRemove = productTags
+          .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+          .map(({ id }) => id);
+        return Promise.all([
+          ProductTag.destroy({ where: { id: productTagsToRemove } }),
+          ProductTag.bulkCreate(newProductTags),
+        ]);
+      }
+      return res.json(product);
     })
-    .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
